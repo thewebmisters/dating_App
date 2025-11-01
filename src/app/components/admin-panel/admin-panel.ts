1caf8e38b9ab81160ac36ce747cc310f9faef494
Nthambu <franklinentamburi@gmail.com>
2025-11-01 14:55:40 +0300

    made changes to UI

---
+++ src/app/components/admin-panel/admin-panel.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-panel',
+  imports: [],
+  templateUrl: './admin-panel.html',
+  styleUrl: './admin-panel.css'
+})
+export class AdminPanel {
+
+}
