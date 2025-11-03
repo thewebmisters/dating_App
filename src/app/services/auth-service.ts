db53a63ddc548d27c43f2f8a37d4827a626f42bf
Nthambu <franklinentamburi@gmail.com>
2025-11-04 00:00:11 +0300

    worked on user details endpoint

--- src/app/services/auth-service.ts
+++ src/app/services/auth-service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
+import { userDetailsBody, UserDTO } from '../data/auth-dto';
 @Injectable({
   providedIn: 'root',
 })
@@ -11,12 +12,29 @@ export class AuthService {
     const fullUrl = `${this.baseUrl}/auth/register`;
     return this.http.post<any>(fullUrl, body);
   }
-  login(body:any):Observable<any>{
+   login(body: any): Observable<any> {
     const fullUrl = `${this.baseUrl}/auth/login`;
-    return this.http.post<any>(
-      fullUrl,
-      body
-    )
-
+    return this.http.post<any>(fullUrl, body).pipe(
+      tap(response => {
+        // This is the crucial part:
+        // After a successful login, store the token.
+        if (response && response.data && response.data.access_token) {
+          localStorage.setItem('access_token', response.data.access_token);
+        }
+      })
+    );
   }
+   //helper method to get stored acess token
+    getAccessToken(){
+      return sessionStorage.getItem('token');
+    }
+    getUserDetails(body?:any):Observable<any>{
+      const fullUrl = `${this.baseUrl}/auth/user`;
+      return this.http.get<any>(fullUrl,body)
+    }
+    //helper function for log out
+    logout(){
+      sessionStorage.removeItem('token');
+     }
+
 }
