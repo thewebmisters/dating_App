e04339a652330df3066859f785c0b4090548c519
Nthambu <franklinentamburi@gmail.com>
2025-11-06 01:06:09 +0300

    Addressing the build failure by modifying angular.json

--- src/app/components/client-home/client-home.ts
+++ src/app/components/client-home/client-home.ts
@@ -34,50 +34,11 @@ export class ClientHome{
       'https://images.unsplash.com/photo-1566753323558-f4e0952af115?auto=format&fit=crop&w=100&q=80',
   };
 imageSrc:string = '/assets/images/mainlogo.jpg';
-  profiles: Profile[] = [
-    {
-      name: 'Maria',
-      age: 28,
-      img: 'https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?auto=format&fit=crop&w=400&q=80',
-      online: true,
-    },
-    {
-      name: 'Sophia',
-      age: 25,
-      img: 'https://images.unsplash.com/photo-1517841905240-472988babdf9?auto=format&fit=crop&w=400&q=80',
-      online: true,
-    },
-    {
-      name: 'Isabella',
-      age: 31,
-      img: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?auto=format&fit=crop&w=400&q=80',
-      online: true,
-    },
-    {
-      name: 'Chloe',
-      age: 27,
-      img: 'https://images.unsplash.com/photo-1580489944761-15a19d654956?auto=format&fit=crop&w=400&q=80',
-      online: true,
-    },
-    {
-      name: 'Elena',
-      age: 29,
-      img: 'https://images.unsplash.com/photo-1611601322175-80415d147128?auto=format&fit=crop&w=400&q=80',
-      online: false,
-    }
-  ];
-  userProfiles:userProfileDTO[]=[];
+ userProfiles:userProfileDTO[]=[];
   fetchProfiles(){
     this.authService.getProfiles().subscribe({
       next:(response)=>{
         this.userProfiles=response;
-        console.log('user profiles',this.userProfiles);
-this.messageService.add({
-              severity: 'error',
-      summary: 'Error',
-      detail:'sucessfully',
-      life: 3000,
-        })
       },
       error:(err)=>{
          this.messageService.add({
