a0dc4c3f7fc55e317f400162b14fb62bdcf3d2c2
Nthambu <franklinentamburi@gmail.com>
2025-12-16 13:59:13 +0300

    made fie

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,8 +1,7 @@
-import { BootstrapContext, bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app/app';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { App } from './app/app'; // Assuming 'App' is your AppComponent
 import { config } from './app/app.config.server';
 
-const bootstrap = (context: BootstrapContext) =>
-    bootstrapApplication(App, config, context);
+const bootstrap = () => bootstrapApplication(App, config);
 
 export default bootstrap;
