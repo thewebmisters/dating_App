7939cfe49688bb1b2c38aa5d7b4cd474613f2690
Nthambu <franklinentamburi@gmail.com>
2025-12-17 11:55:29 +0300

    g

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,3 @@
-import { ClientChat } from './components/client-chat/client-chat';
 import { Routes } from '@angular/router';
 import { LandingPage } from './components/landing-page/landing-page';
 import { Signup } from './components/signup/signup';
@@ -10,24 +9,24 @@ import { ClientHome } from './components/client-home/client-home';
 import { BuyCredit } from './components/buy-credit/buy-credit';
 import { WriterDashboard } from './components/writer-dashboard/writer-dashboard';
 import { ResetPassword } from './components/reset-password/reset-password';
+import { ClientChat } from './components/client-chat/client-chat';
 
 export const routes: Routes = [
-    {path:'landing-page',component:LandingPage},
-    {path:'signup',component:Signup},
-    {path:'login',component:Login},
-     { path:'chat-screen/:id', component: Chatscreen, data: { renderMode: 'server' } },
-    // {path:'chat-screen',component:Chatscreen},
-    {path:'forgot-password',component:ForgotPassword},
-    {path:'admin-panel',component:AdminPanel},
-    {path:'client-home',component:ClientHome},
-    {path:'buy-credit',component:BuyCredit},
-    // {path:'client-chat',component:ClientChat},
-    {path:'client-chat/:id',component:ClientChat,data: { renderMode: 'server' }},
-    {path:'writer-dashboard',component:WriterDashboard},
-    {path:'reset',component:ResetPassword},
-    // Redirect any invalid URL to '/home'
-    {path:'',component:LandingPage},
-    {path:'**',component:LandingPage}
+  { path: 'landing-page', component: LandingPage },
+  { path: 'signup', component: Signup },
+  { path: 'login', component: Login },
+  { path: 'forgot-password', component: ForgotPassword },
+  { path: 'admin-panel', component: AdminPanel },
+  { path: 'client-home', component: ClientHome },
+  { path: 'buy-credit', component: BuyCredit },
+  { path: 'writer-dashboard', component: WriterDashboard },
+  { path: 'reset', component: ResetPassword },
 
+  // Dynamic routes → SSR only
+  { path: 'chat-screen/:id', component: Chatscreen,  data: { renderMode: 'server'}},
+  { path: 'client-chat/:id', component: ClientChat,  data: { renderMode: 'server'} },
+
+  // Default + wildcard
+  { path: '', component: LandingPage },
+  { path: '**', component: LandingPage }
 ];
-
